1de0ae3401e54bfc6089be5353afe5830fd74c80
Deepankar <deepankarsalhan@gmail.com>
2023-12-07 00:05:24 -0700

    navmenu added

--- src/components/navBar/navbar.js
+++ src/components/navBar/navbar.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo2.png';
 // import contactImage from '../../assets/contact.png';
 import contactImageC from '../../assets/address-book-regular.svg';
+import navstrips from '../../assets/bars-solid.svg';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className='navbar'>
       <img src={logo} alt='Logo' className='logo' />
@@ -66,6 +69,63 @@ const Navbar = () => {
         <img src={contactImageC} alt='' className='desktopMenuImg' />
         Contact Me
       </button>
+
+      <img
+        src={navstrips}
+        alt='Menu'
+        className='mobMenu'
+        onClick={() => setShowMenu(!showMenu)}
+      />
+      <div className='navMenu' style={{ display: showMenu ? 'flex' : 'none' }}>
+        <Link
+          activeClass='active'
+          to='intro'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className='ListItem'
+          onClick={() => setShowMenu(false)}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass='active'
+          to='skills'
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className='ListItem'
+          onClick={() => setShowMenu(false)}
+        >
+          About
+        </Link>
+        <Link
+          activeClass='active'
+          to='works'
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className='ListItem'
+          onClick={() => setShowMenu(false)}
+        >
+          Portfolio
+        </Link>
+        <Link
+          activeClass='active'
+          to='contact'
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className='ListItem'
+          onClick={() => setShowMenu(false)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
